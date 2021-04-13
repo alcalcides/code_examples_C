2af0a6a5e4b94c90b2d0f0a749959e8bec310f2f
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2021-04-12 21:03:18 -0300

    Implements Insertion Sort

---
+++ insertionSort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main (void) {
+	const int SIZE = 5;
+	int num[5] = {1, -60, 8, 5, 10};
+	int key, i;
+
+
+	for(int j = 1; j < SIZE; j++) {
+		key = num[j];
+		i = j - 1;
+		while(i >= 0 && num[i] > key) {
+			num[i + 1] = num[i];
+			i--;
+		}
+		num[i + 1] = key;
+	}
+	
+	printf("{");
+	for (int j = 0; j < SIZE; j++) {		
+		printf("%d; ", num[j]);
+
+	}
+	printf("\b\b}\n");
+
+
+	return 0;
+}
