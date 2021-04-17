aaac4f8408bbbe936b9b1b1640a842c598da36bc
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2021-04-17 18:36:42 -0300

    Types conversion

---
+++ ascii.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main (void) {
+    printf("%c\n", 'A');
+    printf("%d\n", 65);
+
+    printf("%c\n", 65);
+    printf("%d\n", 'A');
+    
+    printf("\n");
+
+    printf("%x\n", 16);
+    printf("%d\n", 0x10);
+    
+    printf("%o\n", 8);
+    printf("%d\n", 010);
+    return 0;
+}
