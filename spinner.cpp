951cefe8c562c621f91d406acc20409a709f745b
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2021-04-17 17:16:43 -0300

    C++ sleep thread

---
+++ spinner.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+void sleepPlease(unsigned int microseconds)
+{
+	this_thread::sleep_for(chrono::milliseconds(microseconds));
+}
+
+void printFrame(char glifo, unsigned int microseconds)
+{
+	cout << "\b";
+	cout << glifo;
+	cout.flush();
+	sleepPlease(microseconds);
+}
+
+void printSpin(int numberOfCycles, unsigned int microseconds)
+{
+	for (int i = 0; i < numberOfCycles; i++)
+	{
+		printFrame('|', microseconds);
+		printFrame('/', microseconds);
+		printFrame('-', microseconds);
+		printFrame('\\', microseconds);
+	}
+}
+
+int main(void)
+{
+	const unsigned int microseconds = 80;
+	const int numberOfCycles = 10;
+	printSpin(numberOfCycles, microseconds);
+
+	return 0;
+}
