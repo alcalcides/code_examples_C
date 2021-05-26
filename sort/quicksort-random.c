2eec6a8ad56091ad2490adf0a6c7edc4e17b6ffa
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2021-05-25 23:29:18 -0300

    Removes unwanted console prints

--- sort/quicksort-random.c
+++ sort/quicksort-random.c
@@ -23,11 +23,9 @@ int partition(int *nums, int fromIndex, int untilIndex)
         if (nums[j] <= pivot)
         {
             minorPartitionIndexLimit++;
-            printf("trocando %d por %d\n", nums[minorPartitionIndexLimit], nums[j]);
             swap(&nums[minorPartitionIndexLimit], &nums[j]);
         }
     }
-    printf("trocando %d por %d (fixando o pivo)\n", nums[minorPartitionIndexLimit + 1], nums[untilIndex]);
     swap(&nums[minorPartitionIndexLimit + 1], &nums[untilIndex]);
     return minorPartitionIndexLimit + 1;
 }
@@ -41,15 +39,10 @@ int randomizedPartition(int *nums, int fromIndex, int untilIndex)
 
 void randomizedQuicksort(int *nums, int fromIndex, int untilIndex)
 {
-    printf("sorting\n");
-    printVector(&nums[fromIndex], untilIndex - fromIndex + 1);
     if (fromIndex < untilIndex)
     {
         int indexPivo = randomizedPartition(nums, fromIndex, untilIndex);
-        printf("pivo: [%d] => %d\n", indexPivo, nums[indexPivo]);
-        printf("sort at left FROM index [%d] TO index [%d]\n", fromIndex, indexPivo - 1);
         randomizedQuicksort(nums, fromIndex, indexPivo - 1);
-        printf("sort at right FROM index [%d] TO index [%d]\n", indexPivo + 1, untilIndex);
         randomizedQuicksort(nums, indexPivo + 1, untilIndex);
     }
 }
@@ -58,6 +51,7 @@ int main(void)
 {
     int nums[] = {5, 600, 40, 53, 87, 16};
     int size = sizeof(nums) / sizeof(nums[0]);
+    
     initializeRandomSeed();
 
     printVector(nums, size);
--- sort/quicksort.c
+++ sort/quicksort.c
@@ -10,25 +10,18 @@ int partition(int *nums, int fromIndex, int untilIndex){
     {
         if(nums[j] <= pivot){
             minorPartitionIndexLimit++;
-            printf("trocando %d por %d\n", nums[minorPartitionIndexLimit], nums[j]);
             swap(&nums[minorPartitionIndexLimit], &nums[j]);
         }
     }
-    printf("trocando %d por %d (fixando o pivo)\n", nums[minorPartitionIndexLimit + 1], nums[untilIndex]);
     swap(&nums[minorPartitionIndexLimit+1], &nums[untilIndex]);
     return minorPartitionIndexLimit + 1;
 
 }
 
 void quicksort(int *nums, int fromIndex, int untilIndex){
-    printf("sorting\n");
-    printVector(&nums[fromIndex], untilIndex - fromIndex + 1);
     if(fromIndex < untilIndex){
         int indexPivo = partition(nums, fromIndex, untilIndex);
-        printf("pivo: [%d] => %d\n", indexPivo, nums[indexPivo]);
-        printf("sort at left FROM index [%d] TO index [%d]\n", fromIndex, indexPivo - 1);
         quicksort(nums, fromIndex, indexPivo - 1);
-        printf("sort at right FROM index [%d] TO index [%d]\n", indexPivo + 1, untilIndex);
         quicksort(nums, indexPivo + 1, untilIndex);
     }
 }
@@ -36,10 +29,9 @@ void quicksort(int *nums, int fromIndex, int untilIndex){
 int main(void){
     int nums[] = {5, 60, 8, 90, 40, 53, 87, 16};
     int size = sizeof(nums) / sizeof(nums[0]);
-    printVector(nums, size);
-
-    quicksort(nums, 0, size - 1);
     
+    printVector(nums, size);
+    quicksort(nums, 0, size - 1);    
     printVector(nums, size);
 
 }
